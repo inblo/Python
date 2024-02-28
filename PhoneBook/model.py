b832251c80414027146792a628857aaf452ee4ff
Igor <iblokhin@mail.ru>
2024-02-28 15:09:47 +0300

    New PB

--- PhoneBook/model.py
+++ PhoneBook/model.py
@@ -1,11 +1,11 @@
 phone_book = {}
 path = 'phones.txt'
-SEOARATOR = ';'
+SEPARATOR = ';'
 
 def open_phone_book():
     global phone_book
     with open(path, 'r', encoding='UTF-8') as file:
         data = file.readlines()
-for u_id, contact in enumerate(data, 1):
-    phone_book[u_id] = contact.strip().split(SEOARATOR)
+    for u_id, contact in enumerate(data, 1):
+        phone_book[u_id] = contact.strip().split(SEPARATOR)
 
